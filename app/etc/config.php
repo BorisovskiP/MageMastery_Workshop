53b06ba00502bcd729327183003c3240505fc6de
Petar <petar@customgento.com>
2020-03-20 09:32:59 +0100

    Move files to MageMastery dir

--- app/etc/config.php
+++ app/etc/config.php
@@ -225,6 +225,7 @@ return [
         'Klarna_Kp' => 1,
         'Magento_PaypalReCaptcha' => 1,
         'MSP_TwoFactorAuth' => 1,
+        'MageMastery_Todo' => 1,
         'MagicToolbox_MagicSlideshow' => 1,
         'Temando_Shipping' => 1,
         'Vertex_Tax' => 1,
