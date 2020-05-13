f4a557e889e66ad7f7dd251f005c27309ecefc52
Petar <petar@customgento.com>
2020-05-13 20:39:37 +0200

    Add task methods to data interface

--- app/code/MageMastery/Todo/Model/Task.php
+++ app/code/MageMastery/Todo/Model/Task.php
@@ -10,8 +10,37 @@ use Magento\Framework\Model\AbstractModel;
 
 class Task extends AbstractModel implements TaskInterface
 {
+    public const TASK_ID = 'task_id';
+    public const STATUS = 'status';
+    public const LABEL = 'label';
+
     protected function _construct()
     {
         $this->_init(TaskResource::class);
+    }
+
+    /**
+     * @return int
+     */
+    public function getTaskId(): int
+    {
+        return (int)$this->getData(self::TASK_ID);
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->getData(self::STATUS);
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel(): string
+    {
+        return $this->getData(self::LABEL);
+
     }
 }
