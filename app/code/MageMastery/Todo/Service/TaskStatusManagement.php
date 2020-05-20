056ac081a160f9789c3744d64133e4e71e9d6b7b
Petar <petar@customgento.com>
2020-05-20 08:34:46 +0200

    Add task update api

---
+++ app/code/MageMastery/Todo/Service/TaskStatusManagement.php
@@ -0,0 +1,56 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Todo\Service;
+
+use MageMastery\Todo\Api\TaskManagementInterface;
+use MageMastery\Todo\Api\TaskRepositoryInterface;
+use MageMastery\Todo\Api\TaskStatusManagementInterface;
+use MageMastery\Todo\Model\Task;
+
+class TaskStatusManagement implements TaskStatusManagementInterface
+{
+    /** @var TaskRepositoryInterface */
+    private $repository;
+
+    /**
+     * @var TaskManagementInterface
+     */
+    private $management;
+
+    /**
+     * TaskStatusManagement constructor.
+     *
+     * @param TaskRepository $taskRepository
+     * @param TaskManagement $taskManagement
+     */
+    public function __construct(
+        TaskRepository $taskRepository,
+        TaskManagement $taskManagement
+    ) {
+        $this->management = $taskManagement;
+        $this->repository = $taskRepository;
+    }
+
+    /**
+     * @param int    $taskId
+     * @param string $status
+     *
+     * @return bool
+     */
+    public function change(int $taskId, string $status): bool
+    {
+        if (!in_array($status, ['open', 'complete'])) {
+            return false;
+        }
+
+        $task = $this->repository->get($taskId);
+        $task->setData(Task::STATUS, $status);
+
+        $this->management->save($task);
+
+        return true;
+    }
+
+}
